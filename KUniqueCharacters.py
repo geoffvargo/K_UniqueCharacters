b979229ae0d980f1923164166243885f8c0ccbdb
geoffvargo <geoff.vargo@stonybrook.edu>
2021-10-22 01:35:16 -0400

    INITIAL COMMIT

---
+++ KUniqueCharacters.py
@@ -0,0 +1,13 @@
+from termcolor import colored
+
+
+def KUniqueCharacters(strParam):
+	# code goes here
+	return strParam
+
+
+if __name__ == "__main__":
+	# keep this function call here
+	s = input()
+	print(colored(s, color="green"))
+	print(KUniqueCharacters(s))
