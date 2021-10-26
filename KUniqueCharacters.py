eeb7c35d04aefce533626045016bb93ebcfcdf19
geoffvargo <geoff.vargo@stonybrook.edu>
2021-10-25 22:02:35 -0400

    Abortive attempt at solving problem using a single for-loop.

--- KUniqueCharacters.py
+++ KUniqueCharacters.py
@@ -4,25 +4,45 @@ from termcolor import colored
 def KUniqueCharacters(strParam):
 	# code goes here
 	# print(f'strParam is of {type(strParam)}')
-	k = int(strParam[0])
-	ans = ''
-	
 	# TODO: extract the inner for-loop
+	
+	k = int(strParam[0])
+	
+	print(f'k = {k}')
+	
+	ans = []
+	curr = ''
+	count = 0
+	
 	substr = strParam[1:]
 	for i in range(substr.__len__()):
-		temp = substr[i:]
-		count = 0
-		curr = ''
-		for j in range(temp.__len__()):
-			if temp[j] in curr:
-				curr += temp[j]
-			elif (temp[j] not in curr) and count < k:
-				curr += temp[j]
+		if substr[i] in curr:
+			curr += substr[i]
+		else:
+			if count < k:
+				curr += substr[i]
 				count += 1
-		
-		# print(f'curr = {curr}')
-		if curr.__len__() > ans.__len__():
-			ans = curr
+			else:
+				ans.append(curr)
+				count = 0
+				curr = ''
+	# print(f'curr = {curr}')
+	print(f'ans = {ans}')
+	
+	# for i in range(substr.__len__()):
+	# 	temp = substr[i:]
+	# 	count = 0
+	# 	curr = ''
+	# 	for j in range(temp.__len__()):
+	# 		if temp[j] in curr:
+	# 			curr += temp[j]
+	# 		elif (temp[j] not in curr) and count < k:
+	# 			curr += temp[j]
+	# 			count += 1
+	#
+	# 	# print(f'curr = {curr}')
+	# 	if curr.__len__() > ans.__len__():
+	# 		ans = curr
 	
 	return ans
 
